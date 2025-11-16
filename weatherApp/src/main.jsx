f933efd155ac20fcd623f5ac2836fbc75630fe03
Ahmed <ahmedshariif90@gmail.com>
2025-11-16 14:45:02 -0500

    Rendered the Home component & wrap by provider

--- weatherApp/src/main.jsx
+++ weatherApp/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import Home from './pages/Home.jsx'
+import store from './redux/store.js'
 import './styles/style.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
       <Home />
+    </Provider>
   </StrictMode>,
 )
+
