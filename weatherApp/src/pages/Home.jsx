4f7c76be18a6e98cdf639fd54b6e9cdcb0fcd669
Ahmed <ahmedshariif90@gmail.com>
2025-11-16 14:40:19 -0500

    Rendered the SearchBar & WeatherCard components inside the HomePage

---
+++ weatherApp/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import SearchBar from '../components/SearchBar.jsx'
+import WeatherCard from '../components/WeatherCard.jsx'
+
+
+function Home(){
+    return(
+        <>
+        <SearchBar/>
+        <WeatherCard/>
+        </>
+    )
+}
+
+export default Home
